a750c2b25ffeb502de9a55e0d30f8f1c1b22d308
Julian Guban <jpguban04@gmail.com>
2025-07-30 19:50:12 +0800

    examBtn, answerKey, cssAdjust

--- src/pages/contents/Exam.js
+++ src/pages/contents/Exam.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import tos from "../../assets/Exams/TOS.jpg";
 import topic from "../../assets/Exams/Topics.jpg";
+import file from "../../assets/Exams/EXAM_ELUMBRA.pdf";
 import file1 from "../../assets/Exams/Exam-2.jpg";
 import file2 from "../../assets/Exams/Exam-3.jpg";
 import file3 from "../../assets/Exams/Exam-4.jpg";
@@ -8,11 +9,11 @@ import file4 from "../../assets/Exams/Exam-5.jpg";
 import file5 from "../../assets/Exams/Exam-6.jpg";
 import file6 from "../../assets/Exams/Exam-7.jpg";
 // Answer Key
-// import file7 from "../../assets/Exams/Exam-8.jpg";
-// import file8 from "../../assets/Exams/Exam-1.jpg";
+import file7 from "../../assets/Exams/Exam-8.jpg";
+import file8 from "../../assets/Exams/Exam-1.jpg";
 
 function Exam() {
-  const images = [file1, file2, file3, file4, file5, file6];
+  const images = [file1, file2, file3, file4, file5, file6, file7, file8];
   const [inc, setInc] = useState(0);
 
   const increment = () => {
@@ -54,7 +55,23 @@ function Exam() {
           />
         </div>
       </div>
-      <h2>Exam: </h2>
+
+      <div
+        className="flex-row ppt-header"
+        style={{
+          width: "100%",
+          justifyContent: "space-between",
+          alignItems: "flex-end",
+        }}
+      >
+        <h2>Exam: </h2>
+        <button className="btn btn-primary">
+          <a href={file} target="_blank" rel="noreferrer">
+            Download Exam
+          </a>
+        </button>
+      </div>
+
       <div
         className="flex-row  exam-container"
         style={{ position: "relative", alignItems: "flex-start" }}
@@ -74,6 +91,9 @@ function Exam() {
                 width={"700px"}
                 height={"100%"}
                 loading="lazy"
+                style={{
+                  rotate: (inc === 6 || inc === 7) && "180deg"
+                }}
               />
             </li>
           </ul>
@@ -110,7 +130,7 @@ function Exam() {
                 alt={`Preview ${index}`}
                 width={"60px"}
                 height={"auto"}
-                style={{ objectFit: "cover", borderRadius: "5px" }}
+                style={{ objectFit: "cover", borderRadius: "5px", rotate: (index === 6 || index === 7) && ("180deg") }}
                 loading="lazy"
               />
               <p>Page {index + 1}</p>
